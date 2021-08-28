875c2fc307c070816079a2a9240620ca144691f8
Willian Ferreira dos santos <50432534+willianferreirax@users.noreply.github.com>
2021-08-28 17:49:41 -0300

    Add files via upload

---
+++ webpage/coletar.php
@@ -0,0 +1,20 @@
+<?php
+
+$dir = 'data';
+
+$actions = array('hello', 'thanks', 'iloveyou');
+
+$num_sequences = 30; //quantos videos
+
+$num_frames = 30; //quantos frames cada video
+
+foreach($actions as $action){
+    for($i = 0; $i <= $num_sequences; $i++){
+        try{
+            //crie as pastas
+        }
+        catch(Exception $e){
+            
+        }
+    }
+}
